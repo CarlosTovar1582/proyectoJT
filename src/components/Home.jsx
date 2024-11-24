3457fdcdf83bd2335fdf658480ca493dfb890834
Kalin <78322484+CarlosTovar1582@users.noreply.github.com>
2024-11-23 23:53:35 -0500

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,3 @@
-
 import React from "react";
 
 import { Link } from "react-router-dom";
@@ -14,7 +13,7 @@ const people = [
     email: "3",
     id: 3,
 
-    imageUrl: "https://i.postimg.cc/Hx2PKQdq/facebook-1.png",
+    imageUrl: "https://i.postimg.cc/pL9CH5dr/face-removebg-preview.png",
     href: "https://www.facebook.com/Jeanstovar.pe",
   },
   {
@@ -22,7 +21,7 @@ const people = [
     email: "4",
     id: 4,
 
-    imageUrl: "https://i.postimg.cc/SKwyRb9S/logotipo-de-instagram.png",
+    imageUrl: "https://i.postimg.cc/8cSk1vDS/insta-removebg-preview.png",
     href: "",
   },
   {
@@ -30,7 +29,7 @@ const people = [
     email: "5",
     id: 5,
 
-    imageUrl: "https://i.postimg.cc/2S1RZXrV/tik-tok.png",
+    imageUrl: "https://i.postimg.cc/DyfFBnwB/tik.png",
     href: "",
   },
   // More people...
@@ -39,6 +38,45 @@ const people = [
 export default function Home() {
   return (
     <div className="grid grid-cols-1 gap-4 sm:grid-cols-1 mt-10 mx-4 sm:mx-20 lg:mx-48 xl:mx-96  ">
+      <Link to="/Tienda">
+        <div className="relative flex items-center sm:md:hover:scale-110 md:hover:scale-110  lg:hover:scale-110 space-x-3 rounded-full border border-gray-300 bg-black px-6 py-5 shadow-sm focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 hover:border-gray-400">
+          <div className="shrink-0">
+            <img
+              alt=""
+              src="https://i.postimg.cc/tTb8JN12/flecha3.png"
+              className="size-10 rounded-full bg-white"
+            />
+          </div>
+          <div className="min-w-0 flex-1 ">
+            <span aria-hidden="true" className="absolute inset-0" />
+            <p className=" font-medium text-gray-900 text-center font-Montserrat-Alternates text-white text-1xl">
+              Tienda Virtual
+            </p>
+            <p className="truncate text-sm text-gray-500"></p>
+          </div>
+        </div>
+      </Link>
+
+      <div className="relative flex items-center sm:md:hover:scale-110 md:hover:scale-110  lg:hover:scale-110 space-x-3 rounded-full border border-gray-300 bg-black px-6 py-5 shadow-sm focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 hover:border-gray-400">
+        <div className="shrink-0">
+          <img
+            alt=""
+            src="https://i.postimg.cc/MT0tLkpY/what-removebg-preview.png"
+            className="size-10 rounded-full bg-white"
+          />
+        </div>
+
+        <div className="min-w-0 flex-1 ">
+          <a href="https://api.whatsapp.com/send?phone=962915371">
+            <span aria-hidden="true" className="absolute inset-0" />
+            <p className=" font-medium text-gray-900 text-center font-Montserrat-Alternates text-white text-1xl">
+              Whatssap
+            </p>
+            <p className="truncate text-sm text-gray-500"></p>
+          </a>
+        </div>
+      </div>
+
       {people.map((person) => (
         <div
           key={person.id}
